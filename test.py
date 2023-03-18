4c8360bc17c9a9a4dc687e7f5aecc648badc93c1
Alwaa <74717334+Alwaa@users.noreply.github.com>
2023-03-18 13:18:49 +0100

    Added Spearmint Submodule

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ from comparisson_algs.COBYLA_pilot import COBYLA
 from opt_problems.ADMMBO_paper_problems import gardner1, gardner2
 problem = gardner1
 
-#cma_es(problem)
-COBYLA(problem)
+cma_es(problem)
+#COBYLA(problem)
