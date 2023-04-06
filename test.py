cb48dcd8531d1d96d12684dcdceee3cccd64af98
Alwaa <alexanderpw2001@gmail.com>
2023-04-06 14:00:10 +0200

    Added gridsampling, updated package with utils folder

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from comparisson_algs.cobyla_pilot import cobyla_run
 from comparisson_algs.pesc_script import pesc_main, pesc_run_experiment, pesc_create_problem
 from plotting import vizualize_toy
 from opt_problems.ADMMBO_paper_problems import gardner1, gardner2
+from utils.sampling import monte_carlo_sampling, grid_sampling
 problem = gardner1
 
 # For testting
@@ -28,16 +29,5 @@ if xs != None:
     vizualize_toy(xs,objs,problem,decoupled=False)
 
 
-
-def monte_carlo_sampling(problem, num = 1): #Return list of random ints, not applicable in most algs, as onlt one start point is allowed...
-    bounds = problem["Bounds"]
-    dim_num = len(bounds)//2
-    
-    x0s = np.zeros((num, dim_num))
-    
-    rnd = np.random.RandomState(42 + 1)
-    for i in range(dim_num):
-        x0s[:,i] = rnd.uniform(bounds[2*i], bounds[2*i + 1], num)
-    return x0s
-
 print(monte_carlo_sampling(gardner1, num = 4))
+print(grid_sampling(gardner1))
--- utils/sampling.py
+++ utils/sampling.py
@@ -11,3 +11,30 @@ def monte_carlo_sampling(problem, num = 1): #Return list of random ints, not app
         x0s[:,i] = rnd.uniform(bounds[2*i], bounds[2*i + 1], num)
 
     return x0s
+
+def grid_sampling(problem, num_per_dim = 3):
+    bounds = problem["Bounds"]
+    dim_num = len(bounds)//2
+    
+    arrays = [np.linspace(bounds[2*i],bounds[2*i +1],num_per_dim + 2)[1:-1] for i in range(dim_num)]
+    
+    return cartesian(arrays)
+
+# Taken from https://stackoverflow.com/a/1235363/21573210
+def cartesian(arrays, out=None):     #arrays = [np.asarray(x) for x in arrays]
+    dtype = arrays[0].dtype
+
+    n = np.prod([x.size for x in arrays])
+    if out is None:
+        out = np.zeros([n, len(arrays)], dtype=dtype)
+
+    m = int(n / arrays[0].size)
+    out[:,0] = np.repeat(arrays[0], m)
+    if arrays[1:]:
+        cartesian(arrays[1:], out=out[0:m, 1:])
+        for j in range(1, arrays[0].size):
+            out[j*m:(j+1)*m, 1:] = out[0:m, 1:]
+            
+    return out
+    
+    
