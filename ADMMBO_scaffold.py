Fixed printing the best x-values on experiment plotting and added automatic budget calculation to ADMMBO based on aplha an beta

ADMMBO_scaffold.py
@@ -120,7 +120,7 @@ def admmbo(cost, constraints, M, bounds, grid, x0, f0=None, c0=None,
         mu,std = gpr.predict(x, return_std=True) #TODO:std returns 0? Causes divide by zero
         muu = mu + u_post_pluss(x,zs,ys,rho)
         #xx = (ubest-muu)/std #Original formulation
-        xx = -(muu-ubest)/std #?Why negative here??
+        xx = -(muu-ubest)/(std+1e-10) #?Why negative here?? #Added small value 
         ei = std * (xx * ndtr(xx) + norm.pdf(xx))
         return ei
     
@@ -317,6 +317,7 @@ def admmbo_run(problem, x0, max_iter = 100, admmbo_pars = {}, debugging = False,
 
     costf = problem["Cost Function (x)"]
     constraintf = problem["Constraint Functions (z)"]
+    num_constraints = len(constraintf)
 # --------------------- #
 
     extent_tuple = bounds #bounds are defined as (lb1,ub1,...,lb#,ub#)
@@ -326,7 +327,7 @@ def admmbo_run(problem, x0, max_iter = 100, admmbo_pars = {}, debugging = False,
     # Bound area is then 0 in heatmap
     if M is None:
         func = costf(xy)
-        con = np.all(np.array([constraintf[i](xy) <= 0  for i in range(len(constraintf))]),
+        con = np.all(np.array([constraintf[i](xy) <= 0  for i in range(num_constraints)]),
                 axis = 0) #Boolean constraints
         ff = con*func
         M = np.max(ff) - np.min(ff) # They set it as the unconstrained range of f, while this is the constrained range
@@ -334,33 +335,38 @@ def admmbo_run(problem, x0, max_iter = 100, admmbo_pars = {}, debugging = False,
     
     if not start_all: #Go through all points untill you have one of each constraint both upheld and violated...
         found = False
-        cons_start = np.array([constraintf[c](x0[:1]) <= 0  for c in range(len(constraintf))])
+        cons_start = np.array([constraintf[c](x0[:1]) <= 0  for c in range(num_constraints)])
         for i in range(1,(len(x0)-1)):
             print(i,end = "|", flush = True)
-            #OLDcons_start = np.array([constraintf[c](x0[:i]) <= 0  for c in range(len(constraintf))])
-            cons_add = np.array([constraintf[c](x0[i]) <= 0  for c in range(len(constraintf))]) #Saving on evaluations...
+            #OLDcons_start = np.array([constraintf[c](x0[:i]) <= 0  for c in range(num_constraints)])
+            cons_add = np.array([constraintf[c](x0[i]) <= 0  for c in range(num_constraints)]) #Saving on evaluations...
             cons_start = np.concatenate((cons_start,cons_add), axis = 1)
             #print(cons_start)
             cons_per = np.sum(cons_start, axis = 1)
             more_than_none = np.all((0 < cons_per))
             less_than_all = np.all((i > cons_per))
-            one_fully = np.any((len(constraintf) == np.sum(cons_start, axis=0))) #TODO:Check that this ensures one is fully upheld
+            one_fully = np.any((num_constraints == np.sum(cons_start, axis=0))) #TODO:Check that this ensures one is fully upheld
             if less_than_all and more_than_none and one_fully:
                 x0 = x0[:(i+1)]
                 found = True
                 break
         print("\n")
         if not found and not debugging: #Return the initialization points if failed
-            print("ADMMBO monte carlo initialization did not find feasable strating point set")
+            print("ADMMBO initialization did not find feasable strating point set")
             fs_failed = costf(x0).reshape((-1,1))
-            cons_failed = np.array([constraintf[c](x0)  for c in range(len(constraintf))]).reshape((-1,1))
+            cons_failed = np.array([constraintf[c](x0)  for c in range(num_constraints)]).reshape((-1,1))
             obj_failed = np.concatenate((fs_failed,cons_failed),axis=1)
             return x0, obj_failed, np.full(x0.shape, True)
     
-    K_in = (max_iter-len(x0))//4 #50 #example0 K = 30
+    K_in_old = (max_iter-len(x0))//4 #50 #example0 K = 30
+    ## Calculating ADMMBO budget based on alpha and beta values pluss max_iter
+    a0, a = admmbo_pars.get("alpha0", DEFAULT_OPTIONS["alpha0"]), admmbo_pars.get("alpha", DEFAULT_OPTIONS["alpha"])
+    b0, b = admmbo_pars.get("beta0", DEFAULT_OPTIONS["beta0"]), admmbo_pars.get("beta", DEFAULT_OPTIONS["beta"])
+    K_in = int(1+(max_iter-a0-b0*num_constraints)/(a+b*num_constraints))
+    print(K_in_old, K_in)
     K_in = max(K_in, 2) #At least 2 iterationis (Would be very unlucky for it to fire)
-    #TODO:rewtite to caculate based on alpha and beta
-
+    
+    
     # Grid with evry grid_step-th point of space
     # Should now behave well with non square inputs
     xins = (np.linspace(bounds[i*2], bounds[1+i*2], num_samples)[::grid_step] for i in range(dim_num))

utils/plotting.py
@@ -200,13 +200,14 @@ def expretiment_plot(   exps,
                 #Objfunction rolling min
                 sampl_it = np.arange(len(o1_vals))
                 total_it = np.arange(len(xs))
-                o1_valid =  o1_vals[check_validity(xs_obj)]
-                valid_it = sampl_it[check_validity(xs_obj)]
-                tot_valid_it = total_it[
-                    np.logical_and(check_validity(xs), o1mask)
-                                        ]
+                valid_objxs_mask = check_validity(xs_obj)
+                o1_valid =  o1_vals[valid_objxs_mask]
+                xs_obj_valid = xs_obj[valid_objxs_mask]
+                #valid_it = sampl_it[check_validity(xs_obj)]
+                valid_totxs_mask = check_validity(xs)
+                tot_valid_it = total_it[o1mask][valid_objxs_mask] #total_it[np.logical_and(check_validity(xs), o1mask)]
                 tot_roll_min = np.full(len(total_it), np.nan)
-                smpl_roll_min = np.full(len(sampl_it), np.nan)
+                #smpl_roll_min = np.full(len(sampl_it), np.nan)
                 # print(tot_valid_it,exp_list[0][2])
 
                 for it in range(len(o1_valid)):
@@ -214,13 +215,17 @@ def expretiment_plot(   exps,
 
                 #Rolling min
                 roll_min_len = len(tot_roll_min)
-                a_m = np.argmax(o1_vals)
-                curr_min, cand_x = o1_vals[a_m], xs_obj[a_m]
+                if len(o1_valid) > 0:
+                    a_m = np.argmax(o1_valid)
+                    curr_min, cand_x = o1_valid[a_m], xs_obj_valid[a_m]
+                assert np.all(check_validity(xs_obj_valid)), "fuck"
                 for i in range(1,roll_min_len):
                     if tot_roll_min[i-1] < tot_roll_min[i] or np.isnan(tot_roll_min[i]):
                         tot_roll_min[i] = tot_roll_min[i-1]
-                    if tot_roll_min[i] < curr_min and not np.isnan(tot_roll_min[i]):
-                        cand_x, curr_min = xs[i], tot_roll_min[i]
+                    if tot_roll_min[i-1] < curr_min and not np.isnan(tot_roll_min[i-1]):
+                        cand_x, curr_min = xs[i-1], tot_roll_min[i-1]
+                        if not cand_x in xs_obj_valid:
+                            print("|", end = "-", flush = True)
 
                 candidate_points.append(cand_x);candidate_objs.append(curr_min)
                 roll_min_list.append(tot_roll_min)
@@ -289,7 +294,7 @@ def expretiment_plot(   exps,
         print("\n", alg_name, ":")
         
         for o_i, arr in enumerate(np.round(x_b[top10],decimals=2)):
-            print(list(arr), "\t"*2, top10obj[o_i])
+            print(list(arr), "\t"*2, top10obj[o_i], "\t"*2, check_validity(arr))
     
     plt.figure(1) #Not best practice!
     plt.title(title)